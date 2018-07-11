c05fea6c5ea0626b1349164e6f719b6485d827a6
Shubhodeep Mukherjee <shubhodeep9@gmail.com>
2018-07-11 20:03:29 +0530

    Controller extended

--- Controller/MainController.py
+++ Controller/MainController.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from torn.api.app import Controller
 
-class MainController:
+class MainController(Controller):
     def index():
         return "<h1>Hello World</h1>"
